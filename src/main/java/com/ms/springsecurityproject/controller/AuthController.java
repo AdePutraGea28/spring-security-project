22e4f119f02bf88c427d327388946223cc1eab3e
51206895 <v.ade.gea@adira.co.id>
2023-11-29 17:16:34 +0700

    create auth controller

---
+++ src/main/java/com/ms/springsecurityproject/controller/AuthController.java
@@ -0,0 +1,21 @@
+package com.ms.springsecurityproject.controller;
+
+import com.ms.springsecurityproject.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login() {
+        return ResponseEntity.ok().body(authService.login());
+    }
+}
